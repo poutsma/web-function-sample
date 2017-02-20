041dee0fb69b9d410b3576b80b75adc2abaa332c
Arjen Poutsma <apoutsma@pivotal.io>
2017-02-20 11:25:47 +0100

    Updated sample to latest Spring 5 updates

--- src/main/java/org/springframework/samples/web/reactive/function/Server.java
+++ src/main/java/org/springframework/samples/web/reactive/function/Server.java
@@ -21,6 +21,7 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import reactor.ipc.netty.http.server.HttpServer;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
@@ -29,8 +30,11 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
-import static org.springframework.web.reactive.function.server.RequestPredicates.json;
+import static org.springframework.web.reactive.function.server.RequestPredicates.method;
+import static org.springframework.web.reactive.function.server.RequestPredicates.pathPrefix;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
 
@@ -53,9 +57,11 @@ public class Server {
 		PersonRepository repository = new DummyPersonRepository();
 		PersonHandler handler = new PersonHandler(repository);
 
-		return route(GET("/person/{id}").and(json()), handler::getPerson)
-				.andRoute(GET("/person").and(json()), handler::listPeople)
-				.andRoute(POST("/person").and(contentType(APPLICATION_JSON)), handler::createPerson);
+		return nest(pathPrefix("/person"),
+				nest(accept(APPLICATION_JSON),
+						route(GET("/{id}"), handler::getPerson)
+						.andRoute(method(HttpMethod.GET), handler::listPeople)
+				).andRoute(POST("/").and(contentType(APPLICATION_JSON)), handler::createPerson));
 	}
 
 	public void startReactorServer() throws InterruptedException {
