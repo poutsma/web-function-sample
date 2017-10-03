d58ed06e7b8737f7976d37666ca088b4ebe6f235
Arjen Poutsma <apoutsma@pivotal.io>
2017-10-03 10:40:43 +0200

    Updated sample to Spring 5 GA.

--- src/main/java/org/springframework/samples/web/reactive/function/Server.java
+++ src/main/java/org/springframework/samples/web/reactive/function/Server.java
@@ -47,8 +47,8 @@ public class Server {
 
 	public static void main(String[] args) throws Exception {
 		Server server = new Server();
-//		server.startReactorServer();
-		server.startTomcatServer();
+		server.startReactorServer();
+//		server.startTomcatServer();
 
 		System.out.println("Press ENTER to exit.");
 		System.in.read();
